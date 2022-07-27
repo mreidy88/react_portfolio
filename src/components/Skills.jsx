5ec9d8a140d1f6110d219f5d678ffd92e9c94d36
Mike Reidy <mreidy88@gmail.com>
2022-07-27 16:33:52 -0400

    Skills page troubleshooting, work/portfolio started

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,15 +10,16 @@ import Mongo from '../assets/mongodb.png';
 import Python from '../assets/python.png';
 import RubyOnRails from '../assets/rubyonrails.png'
 
-
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
-        <div className=''>
-            <p className='text-4xl text-gray-300 py-4'>Experience</p>
-            <p>These are the technologies that I work with.</p>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='py-4'>// These are the technologies I've worked with</p>
+          </div>
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
         <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={HTML} alt='HTML5 Logo'></img>
             <p className='my-4'>HTML</p>
@@ -77,12 +78,11 @@ const Skills = () => {
             <img className='w-20 mx-auto' src={RubyOnRails} alt='Ruby On Rails Logo'></img>
             <p className='my-4'>Ruby on Rails</p>
         </div>
-        </div>
 
-
-
+          </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
